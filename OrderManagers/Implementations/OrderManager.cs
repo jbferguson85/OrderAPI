a03a6118f358c5033939fe6474de83febcb0ae55
jbferguson <jbferguson85@outlook.com>
2020-05-25 16:11:23 -0500

    More missing files

---
+++ OrderManagers/Implementations/OrderManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OrderCore.DTOs;
+using OrderManagers.Interfaces;
+using OrderAccessors.Accessors.Interfaces;
+using AutoMapper;
+
+namespace OrderManagers.Implementations
+{
+    public class OrderManager : IOrderManager
+    {
+        private readonly IOrderDataAccessor _orderAccessor;
+        private readonly IMapper _mapper;
+        public OrderManager(IOrderDataAccessor orderDataAccessor, IMapper mapper) 
+        {
+            _orderAccessor = orderDataAccessor;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ProductDto>> GetProductsAsync()
+        {
+            var productEntities = await _orderAccessor.GetProductsAsync();
+            return _mapper.Map<List<ProductDto>>(productEntities);
+        }
+    }
+}
---
+++ OrderManagers/Interfaces/IOrderManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OrderCore.DTOs;
+
+namespace OrderManagers.Interfaces
+{
+    public interface IOrderManager
+    {
+        Task<List<ProductDto>> GetProductsAsync();
+    }
+}
